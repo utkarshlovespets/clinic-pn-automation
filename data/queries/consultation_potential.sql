73c15452cb5ee9b8b77b597b04e36cbef7362072
Utkarsh <utkarsh.ylovespets@supertails.com>
2026-04-08 11:00:23 +0530

    added new cohorts

---
+++ data/queries/consultation_potential.sql
@@ -0,0 +1,26 @@
+SELECT 
+    p.customer_id, 
+    e.customer_name, 
+    ph.phone,
+    p.pincode, 
+    pet.breed
+FROM retentionTeam.vw_cx_pins p
+INNER JOIN retentionTeam.cx_pet_profile pet 
+    ON p.customer_id = pet.customer_id
+INNER JOIN retentionTeam.vw_cx_phones ph 
+    ON p.customer_id = ph.customer_id
+INNER JOIN retentionTeam.vw_cx_email e 
+    ON p.customer_id = e.customer_id
+WHERE p.pincode_city = 'Bangalore' 
+  AND pet.breed IN (
+      'Pug',
+      'French Bulldog',
+      'Pekingese',
+      'German Shepherd',
+      'Labrador Retriever',
+      'Golden Retriever',
+      'Rottweiler',
+      'Great Dane',
+      'Dachshund',
+      'Doberman'
+  );
